89b86ac94cba1c5daf41e786a8386ef91954d9d1
Mochamad Surya Rafliansyah <89367252+rafli21xrplc@users.noreply.github.com>
2023-08-29 13:06:23 +0700

    validation songs playlist

--- resources/views/users/playlist/contoh.blade.php
+++ resources/views/users/playlist/contoh.blade.php
@@ -2,64 +2,71 @@
 
 @section('content')
     <link rel="stylesheet" href="/user/assets/css/contohPlaylist.css">
+    <link rel="stylesheet" href="/user/assets/css/buatPlaylist.css">
     <div class="main-panel">
         <div class="content-wrapper">
             <div class="row">
                 <div class="col-md-12 stretch-card">
                     <div class="custom-container">
                         <div class="row">
-                            <div class="col-3">
-                                <div class="card coba">
+                            @if ($playlistDetail->user_id === auth()->user()->id)
+                                <div class="col-3">
+                                    <a href="#popup" class="card coba">
+                                        <img src="{{ asset('storage/' . $playlistDetail->images) }}" alt="Gambar">
+                                    </a>
+                                </div>
+                            @else
+                                <div class="col-3">
                                     <img src="{{ asset('storage/' . $playlistDetail->images) }}" alt="Gambar">
                                 </div>
-                            </div>
-                            <div class="col-3 text-xxl-end">
-                                <div class="bottom-left-text">
-                                    <p class="m-0" style="font-size: 18px; font-weight: 500">{{ $playlistDetail->name }}
-                                    </p>
-                                    <h3 style="font-size: 18px; font-weight: 600">
-                                        {{ $playlistDetail->deskripsi == 'none' ? '' : "$playlistDetail->deskripsi" }}
-                                    </h3>
-                                </div>
+                            @endif
+                        </div>
+                        <div class="col-3 text-xxl-end">
+                            <div class="bottom-left-text">
+                                <p class="m-0" style="font-size: 18px; font-weight: 500">{{ $playlistDetail->name }}
+                                </p>
+                                <h3 style="font-size: 18px; font-weight: 600">
+                                    {{ $playlistDetail->deskripsi == 'none' ? '' : "$playlistDetail->deskripsi" }}
+                                </h3>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12">
-                    <hr class="divider"> <!-- Divider -->
-                </div>
-                <div class="col-md-12 grid-margin stretch-card">
-                    <h3 class="card-title judul">Temukan berbagai lagu</h3>
-                    <form class="col-6 mb-4 p-0 nav-link search">
-                        <input type="text" class="form-control rounded-4" placeholder="Cari musik">
-                    </form>
-                    <div class="card scroll scrollbar-down thin">
-                        <div class="card-body">
-                            <div class="row" style="margin-top: -20px">
-                                <div class="col-12">
-                                    <div class="preview-list">
-                                        @foreach ($songs as $item)
-                                            <div class="preview-item">
-                                                <div class="preview-thumbnail">
-                                                    <img src="{{ asset('storage/' . $item->image) }}" width="10%">
+            </div>
+            <div class="col-md-12">
+                <hr class="divider"> <!-- Divider -->
+            </div>
+            <div class="col-md-12 grid-margin stretch-card">
+                <h3 class="card-title judul">Temukan berbagai lagu</h3>
+                <form class="col-6 mb-4 p-0 nav-link search">
+                    <input type="text" class="form-control rounded-4" placeholder="Cari musik">
+                </form>
+                <div class="card scroll scrollbar-down thin">
+                    <div class="card-body">
+                        <div class="row" style="margin-top: -20px">
+                            <div class="col-12">
+                                <div class="preview-list">
+                                    @foreach ($songs as $item)
+                                        <div class="preview-item">
+                                            <div class="preview-thumbnail">
+                                                <img src="{{ asset('storage/' . $item->image) }}" width="10%">
+                                            </div>
+                                            <div class="preview-item-content d-sm-flex flex-grow">
+                                                <div class="flex-grow">
+                                                    <h6 class="preview-subject">{{ $item->judul }}</h6>
+                                                    <p class="text-muted mb-0">{{ $item->artist->user->name }}</p>
                                                 </div>
-                                                <div class="preview-item-content d-sm-flex flex-grow">
-                                                    <div class="flex-grow">
-                                                        <h6 class="preview-subject">{{ $item->judul }}</h6>
-                                                        <p class="text-muted mb-0">{{ $item->artist->user->name }}</p>
-                                                    </div>
-                                                    <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                                                        <div class="text-group">
-                                                            <i onclick="myFunction(this)" class="far fa-heart pr-2">
-                                                            </i>
-                                                            <p>{{ $item->waktu }}</p>
-                                                            <i class="fas fa-ellipsis-v"></i>
-                                                        </div>
+                                                <div class="mr-auto text-sm-right pt-2 pt-sm-0">
+                                                    <div class="text-group">
+                                                        <i onclick="myFunction(this)" class="far fa-heart pr-2">
+                                                        </i>
+                                                        <p>{{ $item->waktu }}</p>
+                                                        <i class="fas fa-ellipsis-v"></i>
                                                     </div>
                                                 </div>
                                             </div>
-                                        @endforeach
-                                    </div>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
@@ -69,6 +76,39 @@
         </div>
     </div>
     </div>
+    </div>
+    </div>
+
+    <div id="popup">
+        <div class="card window">
+            <div class="card-body">
+                <a href="#" class="close-button mdi mdi-close-circle-outline"></a>
+                <h3 class="judul">Buat Playlist</h2>
+                    <form class="row" action="{{ route('buat.playlist') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="col-4">
+                            <div class="card cobai">
+                                <label for="gambar" id="tampil_gambar">
+                                    <i class="fas fa-pen fa-2x"></i>
+                                </label>
+                                <input type="file" id="gambar" name="images" accept="image/png,image/jpg">
+                            </div>
+                        </div>
+                        <div class="col-md-7 ml-4">
+                            <div class="mb-3">
+                                <input type="text" class="form-control form-i" name="name" id="nama"
+                                    placeholder="Judul Playlist">
+                            </div>
+                            <div class="mb-3">
+                                <textarea id="deskripsi" class="form-control" name="deskripsi" maxlength="500" rows="6" placeholder="Deskripsi"></textarea>
+                            </div>
+                        </div>
+                        <div class="text-md-right">
+                            <button class="btn" type="submit">Simpan</button>
+                        </div>
+                    </form>
+            </div>
+        </div>
     </div>
 
     <script>
