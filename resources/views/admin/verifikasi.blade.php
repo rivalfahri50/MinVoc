9a66b094cf22af7cb429f40d0553a091b3401a61
rivalfahri <130647114+rivalfahri50@users.noreply.github.com>
2023-09-15 10:55:20 +0700

    Merge branch 'main' of

--- resources/views/admin/verifikasi.blade.php
+++ resources/views/admin/verifikasi.blade.php
@@ -19,7 +19,8 @@
                                     <h5 class="judul mb-3">Nama :</h5>
                                     <td class="table-cell">
                                         <div class="cell-content">
-                                            <img src="{{ asset('storage/' . $item->user->avatar) }}" alt="Face" class="avatar">
+                                            <img src="{{ asset('storage/' . $item->user->avatar) }}" alt="Face"
+                                                class="avatar">
                                             <div>
                                                 <p class="teksbiasa">{{ $item->user->name }}</p>
                                             </div>
